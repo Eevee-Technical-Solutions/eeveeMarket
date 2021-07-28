88dc69e72472b6a1c96e8598a2e7020e166b9f5b
flin1105 <flin1105@DESKTOP-BFV96NP.localdomain>
2021-07-27 20:59:47 -0400

    Adding getWinner and bidItem controller

--- src/server/routes/auctionRouter.ts
+++ src/server/routes/auctionRouter.ts
@@ -10,11 +10,11 @@ router.get("/homepage", auctionController.getItem, (req: any, res: any, next: an
 })
 
 router.post("/bid", auctionController.bidItem, (req: any, res: any, next: any)=> {
-    return res.status(200).json(res.locals)
+    return res.status(200).json("bidPrice has been updated")
  })
 
 router.get("/winner", auctionController.getWinner, auctionController.getItem, (req: any, res: any, next: any)=> {
-  return res.status(200).json(res.locals)
+  return res.status(200).json([res.locals.winner, res.locals.item])
 })
 
 module.exports = router;
