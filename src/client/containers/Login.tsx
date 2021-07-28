2a6ae910d51d4d0baa045e63e4f53ff037fb27b0
flin1105 <flin1105@DESKTOP-BFV96NP.localdomain>
2021-07-28 08:08:47 -0400

    Adding initial passport setup

---
+++ src/client/containers/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const Login = () : any => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <a href="/auth/github">login with github</a>
+    </div>
+  )
+}
+  
